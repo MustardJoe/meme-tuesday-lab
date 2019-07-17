b0e6f7603824df4b02bfa0c69e1f18ccd71fd609
Jonathan Forney <jonathan.forney@gmail.com>
2019-07-16 17:07:49 -0700

    got post and basic get test and func working

---
+++ test/app.test.js
@@ -0,0 +1,53 @@
+require('dotenv').config();
+
+const request = require('supertest');
+const app = require('../lib/app');
+const connect = require('../lib/utils/connects');
+const mongoose = require('mongoose');
+const Meme = require('../lib/models/Meme');
+
+describe('meme routing and middleware', () => {
+  beforeAll(() => {
+    connect();
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
+  it('is a test to post a new meme', () => {
+    return request(app)
+      .post('/api/v1/memes')
+      .send({ 
+        top: 'shit, I would eat that',
+        image: 'https://prods3.imgix.net/images/articles/2017_04/Feature-restaurant-butcher-bakery-shops2.jpg?auto=format%2Ccompress&ixjsv=2.2.3',
+        bottom: 'Sandwich life'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          top: 'shit, I would eat that',
+          image:'https://prods3.imgix.net/images/articles/2017_04/Feature-restaurant-butcher-bakery-shops2.jpg?auto=format%2Ccompress&ixjsv=2.2.3',
+          bottom: 'Sandwich life',
+          __v: 0
+          
+        });
+      });
+
+  });
+
+  it('gets (an array of?) all the memes', async() => {
+    const meme = await Meme.create({ image: 'rap bone url', bottom: 'oh shit' });
+
+    return request(app)
+      .get('/api/v1/memes')
+      .then(res => {
+        const memeJSON = JSON.parse(JSON.stringify(meme));
+        expect(res.body).toEqual([memeJSON]);
+      });
+  });
+});
